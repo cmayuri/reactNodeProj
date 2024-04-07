ea509921cf450c29a48c3635ad32c061214c418d
Mayuri Chavan <cmayuri1996@gmail.com>
2024-04-07 19:56:58 +0530

    changes

--- backend/Config/dbconfig.js
+++ backend/Config/dbconfig.js
@@ -1,8 +1,8 @@
 module.exports ={
-    HOST : 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'node_sequelize_api_db',
+    HOST : process.env.DB_HOST ||'localhost',
+    USER: process.env.DB_USER||'root',
+    PASSWORD: process.env.DB_PASSWORD ||'',
+    DB: process.env.DB_NAME || 'node_sequelize_api_db',
     dialect: 'mysql',
 
     pool:{
